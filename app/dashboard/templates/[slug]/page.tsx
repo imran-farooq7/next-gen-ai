9b713ceae2cc7253f0d67c934738939ea87513e8
Imran Farooq <imran_farooq7@hotmail.com>
2024-09-25 04:51:49 +0500

    text editor component added

--- app/dashboard/templates/[slug]/page.tsx
+++ app/dashboard/templates/[slug]/page.tsx
@@ -2,7 +2,12 @@
 import { generateText } from "@/lib/actions/actions";
 import Templates from "@/lib/constants";
 import Image from "next/image";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
+
 import { useState } from "react";
+import Link from "next/link";
+import { ArrowLeftIcon } from "@heroicons/react/16/solid";
 interface Template {
 	name: string;
 	desc: string;
@@ -42,54 +47,70 @@ const TemplatePage = ({ params: { slug } }: Props) => {
 	};
 
 	return (
-		<div className="grid grid-cols-1 md:grid-cols-3 gap-5 px-5">
-			<div className="col-span-1 rounded-md shadow-lg border p-4">
-				<div className="flex flex-col gap-4">
-					<Image
-						src={template.icon}
-						alt={template.name}
-						width={50}
-						height={50}
-					/>
-					<h2 className="font-medium text-lg">{template.name}</h2>
-					<p className="text-gray-400">{template.desc}</p>
+		<div className="flex flex-col items-start">
+			<Link
+				href={"/dashboard"}
+				className=" bg-emerald-500 gap-2 transition-all ease-in-out flex items-center my-4 text-white px-8 hover:scale-105 py-3 rounded-lg"
+			>
+				<ArrowLeftIcon className="w-5 h-5" />
+				<span>Back</span>
+			</Link>
+			<div className="grid grid-cols-1 md:grid-cols-3 gap-5 px-5">
+				<div className="col-span-1 rounded-md shadow-lg border p-4">
+					<div className="flex flex-col gap-4">
+						<Image
+							src={template.icon}
+							alt={template.name}
+							width={50}
+							height={50}
+						/>
+						<h2 className="font-medium text-lg">{template.name}</h2>
+						<p className="text-gray-400">{template.desc}</p>
+					</div>
+					<form onSubmit={handleSubmit}>
+						{template.form.map((field) => (
+							<div key={field.label} className="my-2 flex flex-col">
+								<label className="font-medium pb-5" key={field.label}>
+									{field.label}
+								</label>
+								{field.field === "input" ? (
+									<input
+										type="text"
+										name={field.name}
+										placeholder={field.name}
+										onChange={(e) => setQuery(e.target.value)}
+										value={query}
+										required={field.required}
+										className="w-full border shadow-md outline-none text-black dark:text-white placeholder-gray-500 dark:placeholder-gray-400 p-2"
+									/>
+								) : (
+									<textarea
+										rows={4}
+										value={query}
+										onChange={(e) => setQuery(e.target.value)}
+										className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+										defaultValue={""}
+									/>
+								)}
+							</div>
+						))}
+						<button
+							className="disabled:cursor-wait bg-emerald-500 my-4 text-white w-full px-4 py-3 rounded-lg"
+							type="submit"
+						>
+							{isLoading ? "Generating..." : "Generate"}
+						</button>
+					</form>
 				</div>
-				<form onSubmit={handleSubmit}>
-					{template.form.map((field) => (
-						<div key={field.label} className="my-2 flex flex-col">
-							<label className="font-medium pb-5" key={field.label}>
-								{field.label}
-							</label>
-							{field.field === "input" ? (
-								<input
-									type="text"
-									name={field.name}
-									placeholder={field.name}
-									onChange={(e) => setQuery(e.target.value)}
-									value={query}
-									required={field.required}
-									className="w-full border shadow-md outline-none text-black dark:text-white placeholder-gray-500 dark:placeholder-gray-400 p-2"
-								/>
-							) : (
-								<textarea
-									rows={4}
-									value={query}
-									onChange={(e) => setQuery(e.target.value)}
-									className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-									defaultValue={""}
-								/>
-							)}
-						</div>
-					))}
-					<button
-						className="disabled:cursor-wait bg-emerald-500 my-4 text-white w-full px-4 py-3 rounded-lg"
-						type="submit"
-					>
-						{isLoading ? "Generating..." : "Generate"}
-					</button>
-				</form>
+				<div className="col-span-2">
+					<ReactQuill
+						placeholder="Your AI generated appear here"
+						value={aiConent}
+						className="h-full"
+						formats={["content"]}
+					/>
+				</div>
 			</div>
-			{aiConent ?? "Ai conent render here"}
 		</div>
 	);
 };
