3b2fd596cb688039b00363c259be4187d98071eb
Imran Farooq <imran_farooq7@hotmail.com>
2024-09-25 19:51:33 +0500

    history page partially completed

---
+++ app/dashboard/history/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+const HistoryPage = () => {
+	return (
+		<div className="p-10 my-5 mx-5 mb-5 rounded-lg dark:bg-gray-900 flex flex-col justify-center items-center">
+			<h1 className="text-xl font-bold">Histroy</h1>
+			<p>Your search history</p>
+		</div>
+	);
+};
+
+export default HistoryPage;
